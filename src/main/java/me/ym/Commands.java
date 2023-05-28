50c4aa0c30aa2211ad67d3c0770d2d0016371e71
JustM1k3 <73421470+JustM1k3@users.noreply.github.com>
2023-05-28 19:39:57 +0200

    Initial commit

---
+++ src/main/java/me/ym/Commands.java
@@ -0,0 +1,14 @@
+package me.ym;
+
+import me.oxolotel.utils.wrapped.command.Command;
+import me.oxolotel.utils.wrapped.command.sender.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class Commands implements Command{
+    @Override
+    public boolean execute(@NotNull CommandSender commandSender, @NotNull List<String> list, @NotNull List<String> list1) {
+        return false;
+    }
+}
---
+++ src/main/java/me/ym/Main.java
@@ -0,0 +1,16 @@
+package me.ym;
+
+import me.oxolotel.utils.wrapped.plugin.BukkitPlugin;
+
+public final class Main extends BukkitPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
