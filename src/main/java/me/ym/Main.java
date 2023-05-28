df17607f6189f449329d4ba1d00e6016ffbca234
JustM1k3 <73421470+JustM1k3@users.noreply.github.com>
2023-05-28 20:38:57 +0200

    add Commands

--- src/main/java/me/ym/Main.java
+++ src/main/java/me/ym/Main.java
@@ -1,12 +1,15 @@
 package me.ym;
 
 import me.oxolotel.utils.wrapped.plugin.BukkitPlugin;
+import me.oxolotel.utils.wrapped.plugin.Plugin;
+import org.bukkit.command.Command;
 
 public final class Main extends BukkitPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        new Commands().register(Plugin.of(this));
     }
 
     @Override
